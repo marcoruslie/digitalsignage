50ce252358f76743ce03bcd4d75131fda3983eb5
Albertus Marco <99940477+marcoruslie@users.noreply.github.com>
2024-09-30 11:51:47 +0700

    Update [...].ts

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -127,7 +127,7 @@ io.on("connection", async (socket) => {
           kategori: item.category.cat_name.replace(" ", ""),
           fileUrl: passUrl,
           an_type: ann.announcement.an_type,
-          an_fileName: ann.announcement.an_url.split("\\").pop(),
+          an_fileName: ann.announcement.an_url.split("/").pop(),
           fileData: data.toString("base64"),
         };
         fileData.push(annData);
