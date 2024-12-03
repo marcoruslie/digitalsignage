4de9d1f10f41c1bdd8f09a1903a8bc30aa0109b9
Albertus Marco <99940477+marcoruslie@users.noreply.github.com>
2024-12-03 12:21:26 +0700

    Update MasterScreenPage.vue

--- pages/Admin/MasterScreenPage.vue
+++ pages/Admin/MasterScreenPage.vue
@@ -155,9 +155,8 @@
 				<div>Select Screen</div>
 				<select v-if="listReminder != null && listReminder.length > 0" class="w-full rounded py-2 border mb-4"
 					v-model="reminderScreenIp" @change="reminderData">
-					<option v-if="ScreenDetail(ip.socket_ip)"
-						v-for="ip in listReminder.filter((item) => item.socket_ip != '')" :value="ip.socket_ip"
-						class="text-onPrimary">
+					<option v-for="ip in listReminder.filter((item) => item.socket_ip != '')" :value="ip.socket_ip"
+						v-if="ScreenDetail(ip.socket_ip)" class="text-onPrimary">
 						<div v-if="ScreenDetail(ip.socket_ip)">{{
 							ip.socket_ip + " - " + ScreenDetail(ip.socket_ip).sc_name + " - " +
 							ScreenDetail(ip.socket_ip).sc_location }}</div>
